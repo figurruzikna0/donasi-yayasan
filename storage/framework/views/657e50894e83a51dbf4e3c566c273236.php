4ba57580a2cf4796307ffb269504352febe52371
figurruzikna0 <figurruzikna7@gmail.com>
2026-07-15 03:42:32 +0700

    chore: ignore compiled blade views, hapus dari tracking

--- storage/framework/views/657e50894e83a51dbf4e3c566c273236.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'btn btn-neutral'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\laragon\www\donasi-yayasan\resources\views/components/primary-button.blade.php ENDPATH**/ ?>
